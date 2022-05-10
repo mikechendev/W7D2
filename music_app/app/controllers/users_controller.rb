ac85017cedcaa69960593bfd20267c44138166bb
Michael Chen <michael.chen.1120@gmail.com>
2022-05-10 11:16:52 -0400

    add routes move spire methods to user model

--- music_app/app/controllers/users_controller.rb
+++ music_app/app/controllers/users_controller.rb
@@ -1,33 +1,23 @@
 class UsersController < ApplicationController
 
-    attr_reader :password
-    before_validation :ensure_session_token
+    def new
+        @user = User.new
+        render :new
+    end
 
-    def self.find_by_credentials(email, password)
-        user = User.find_by(email: email)
-        if user && user.is_password?(password)
-            user
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to user_url(@user)
         else
-            nil
+            flash[:errors] = @user.errors.full_messages
+            redirect_to new_user_url
         end
     end
 
-    def password=(password)
-        self.password_digest = BCrypt::Password.create(password)
-        @password = password
-    end
+    private
 
-    def is_password?(password)
-        BCrypt::Password.new(self.password_digest).is_password?(password)
-    end
-
-    def reset_session_token!
-        self.session_token = SecureRandom::urlsafe_base64
-        self.save!
-        self.session_token
-    end
-
-    def ensure_session_token
-        self.session_token ||= SecureRandom::urlsafe_base64
+    def user_params
+        params.require(:user).permit(:email, :password, :session_token)
     end
 end
